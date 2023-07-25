891ebd7f77be311a6737b89dfddf739d85bba9b3
Anna <annfyz1232@yandex.ru>
2023-07-25 08:33:50 +0200

    fixed review

--- Stairs_2D_Game/Assets/Scripts/CardController.cs
+++ Stairs_2D_Game/Assets/Scripts/CardController.cs
@@ -26,23 +26,14 @@ public class CardController : MonoBehaviour
             isCardActivated = true;
         }
     }
-    public static CardController Create(Transform prefabCard, Transform parent)
-    {
 
-        Transform cardObj = Instantiate(prefabCard);
-        cardObj.transform.SetParent(parent);
-        CardController card = prefabCard.GetComponent<CardController>();
-        //Canvas.ForceUpdateCanvases();
-        return card;
-    }
-
-    public void Setup(CardGroup_SO group, Assignment typeOfAssignment, DiffAssignments assignments)
+    public void Setup(CardGroup_SO group, Assignment typeOfAssignment, DiffAssignments assignments, int index)
     {
-        //assignmentType = group.TypeOfAssignment;
-        // assingnment = group.assignments;
         assignmentType = typeOfAssignment;
         assingnment = assignments;
         this.GetComponentInChildren<Image>().color = group.groupColor;
+        //cardGroupIndex = group.numberOfAssignmentGroup;
+        cardGroupIndex = index;
     }
 
 
@@ -56,14 +47,17 @@ public class CardController : MonoBehaviour
             if (assignmentType == Assignment.Assignment_With_Answer_Options && assingnment.assignmentWithAnswers != null)
             {
                 UI_Assignment_With_Answers.Instance.ActivateUIPanel(assingnment.assignmentWithAnswers.Question, assingnment.assignmentWithAnswers.Answers[0], assingnment.assignmentWithAnswers.Answers[1], assingnment.assignmentWithAnswers.Answers[2], assingnment.assignmentWithAnswers.sprite);
+
             }
             else if (assignmentType == Assignment.Assignment_With_Number_Input)
             {
                 UI_Assignment_WithInput.Instance.ActivateUIPanel(assingnment.assignmentWithUserInput_Number.Question, assingnment.assignmentWithUserInput_Number.sprite);
+
             }
             else if (assignmentType == Assignment.Assignment_With_Text_Input)
             {
                 UI_Assignment_WithInput.Instance.ActivateUIPanel(assingnment.assignmentWithUserInput_Text.Question, assingnment.assignmentWithUserInput_Text.sprite);
+
             }
         }
     }
--- Stairs_2D_Game/Assets/Scripts/CardManager.cs
+++ Stairs_2D_Game/Assets/Scripts/CardManager.cs
@@ -22,15 +22,6 @@ public class CardManager : MonoBehaviour
 
     int groupIndex = 0;
 
-    [SerializeField] DiffAssignments allAssignments;
-    public int indexForAssignmentsWithAnswers = 0;
-    public int indexForAssignmentsWithUserInput_Numbers = 0;
-    public int indexForAssignmentsWithUserInput_Text = 0;
-
-    public bool allCardsWithAnswersWereCreated = false;
-    public bool allCardsWithUserInput_Numbers_WereCreated = false;
-    public bool allCardsWithUserInput_Text_WereCreated = false;
-
     private void Awake()
     {
 
@@ -42,6 +33,10 @@ public class CardManager : MonoBehaviour
         {
             Instance = this;
         }
+    }
+    private void Update()
+    {
+        currentselectedCard = selectedCard;
     }
     void OnEnable()
     {
@@ -77,8 +72,8 @@ public class CardManager : MonoBehaviour
 
         //CheckTheFirstCard();
         AddAllCreatedCards();
-       // ReorganizeCreatedCards();
-        //selectedCard = reorginizedCards[0];
+        ReorganizeCreatedCards();
+        selectedCard = reorginizedCards[0];
         ResultsHandler.Instance.CalculateAmountOfAllAnswers();
     }
 
@@ -117,20 +112,13 @@ public class CardManager : MonoBehaviour
 
             foreach (var group in CardGroups)
             {
-                
-
-                AddAllAssignments(group);
 
                 for (int i = 0; i < group.currentAmountOfCardsOfThisType; i++)
                 {
-                    //GetAssignment(group, i);
+                   
                     group.GetAssignment();
                     Transform card =  Instantiate(prefabCard, transform);
-                    card.GetComponent<CardController>().Setup(group, group.TypeOfAssignment, group.assignments);
-
-                    //card = CardController.Create(prefabCard, transform);
-                    //Setup(group, group.TypeOfAssignment, group.assignments);                  
-
+                    card.GetComponent<CardController>().Setup(group, group.TypeOfAssignment, group.assignments, groupIndex);
                 }
 
                 if (groupIndex < CardGroups.Length)
@@ -142,123 +130,7 @@ public class CardManager : MonoBehaviour
 
     }
 
-    void CreateCards()
-    {
-
-    }
-
-    void AddAllAssignments(CardGroup_SO group)
-    {
-      
-
-        if (group.TypeOfAssignment == Assignment.Assignment_With_Answer_Options)
-        {
-            Debug.Log("Add  " + Assignment.Assignment_With_Answer_Options);
-            allAssignments.AssignmentsWithAnswers = group.assignments.AssignmentsWithAnswers;
-        }
-        if (group.TypeOfAssignment == Assignment.Assignment_With_Number_Input)
-        {
-            Debug.Log("Add  " + Assignment.Assignment_With_Number_Input);
-            allAssignments.AssignmentsWithUserInput_Number = group.assignments.AssignmentsWithUserInput_Number;
-        }
-        if (group.TypeOfAssignment == Assignment.Assignment_With_Text_Input)
-        {
-            Debug.Log("Add  " + Assignment.Assignment_With_Text_Input);
-            allAssignments.AssignmentsWithUserInput_Text = group.assignments.AssignmentsWithUserInput_Text;
-        }
-    }
-
-
-    public void GetAssignment(CardGroup_SO group, int index)
-    {
-        if (group.TypeOfAssignment == Assignment.Assignment_With_Answer_Options)
-        {
-            SelectAssignmentWithAnswers(index);
-        }
-        else if (group.TypeOfAssignment == Assignment.Assignment_With_Number_Input)
-        {
-            SelectAssignmentWithUserInput_Number(index);
-        }
-        else if (group.TypeOfAssignment == Assignment.Assignment_With_Text_Input)
-        {
-            SelectAssignmentWithUserInput_Text(index);
-        }
-
-    }
-
-    void SelectAssignmentWithAnswers(int index)
-    {
-        if (indexForAssignmentsWithAnswers < allAssignments.AssignmentsWithAnswers.Length)
-        {
-            //Debug.Log("indexForAssignmentsWithAnswers " + indexForAssignmentsWithAnswers);
-            allAssignments.assignmentWithAnswers = allAssignments.AssignmentsWithAnswers[index];
-            
-
-        }
-        if (index == allAssignments.AssignmentsWithAnswers.Length)
-        {
-            allCardsWithAnswersWereCreated = true;
-            allAssignments.assignmentWithAnswers = null;
-            //assignments.assignmentWithAnswers = assignments.AssignmentsWithAnswers[assignments.AssignmentsWithAnswers.Length-1];
-
-        }
-
-        //Debug.Log(" randomAssignment.assignmentWithAnswers " + randomAssignment.assignmentWithAnswers);
-
-    }
-
-    void SelectAssignmentWithUserInput_Number(int index)
-    {
-        if (indexForAssignmentsWithUserInput_Numbers < allAssignments.AssignmentsWithUserInput_Number.Length)
-        {
-            //Debug.Log("indexForAssignmentsWithUserInput_Numbers " + indexForAssignmentsWithUserInput_Numbers);
-            allAssignments.assignmentWithUserInput_Number = allAssignments.AssignmentsWithUserInput_Number[indexForAssignmentsWithUserInput_Numbers];
-        
-
-        }
-        if (index == allAssignments.AssignmentsWithUserInput_Number.Length)
-        {
-            allCardsWithUserInput_Numbers_WereCreated = true;
-            allAssignments.assignmentWithUserInput_Number = null;
-            //assignments.assignmentWithUserInput_Number = assignments.AssignmentsWithUserInput_Number[assignments.AssignmentsWithUserInput_Number.Length -1];
-        }
-
-        //Debug.Log("randomAssignment.assignmentWithUserInput_Number " + randomAssignment.assignmentWithUserInput_Number);
-
-    }
-
-    void SelectAssignmentWithUserInput_Text(int index)
-    {
-        if (indexForAssignmentsWithUserInput_Text < allAssignments.AssignmentsWithUserInput_Text.Length)
-        {
-            //Debug.Log("indexForAssignmentsWithUserInput_Text " + indexForAssignmentsWithUserInput_Text);
-            allAssignments.assignmentWithUserInput_Text = allAssignments.AssignmentsWithUserInput_Text[indexForAssignmentsWithUserInput_Text];
-            
-        }
-        if (index == allAssignments.AssignmentsWithUserInput_Text.Length)
-        {
-            allCardsWithUserInput_Text_WereCreated = true;
-            allAssignments.assignmentWithUserInput_Text = null;
-            //assignments.assignmentWithUserInput_Text = assignments.AssignmentsWithUserInput_Text[assignments.AssignmentsWithUserInput_Text.Length -1];
-        }
-
-        //Debug.Log("randomAssignment.assignmentWithUserInput_Text " + randomAssignment.assignmentWithUserInput_Text);
-
-    }
-
-
-    //void CheckTheFirstCard()
-    //{
-    //    CardController firstCard = GetComponentInChildren<CardController>();
-    //    firstCard.Setup(CardGroups[0]);
-
-
-    //    for (int i = 0; i < createdCards.Length; i++)
-    //    {
-    //        //createdCards.Contains<>
-    //    }
-    //}
-
+   
     void AddAllCreatedCards()
     {
         createdCards = GetComponentsInChildren<CardController>();
